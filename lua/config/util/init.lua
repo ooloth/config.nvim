e26e9a829b582de4e8be2c09d3c9c0339bb1e00a
Michael Uloth <hello@michaeluloth.com>
2025-02-20 20:50:53 -0500

    utils: remove unused helper

--- lua/config/util/init.lua
+++ lua/config/util/init.lua
@@ -1,11 +1,7 @@
 local M = {}
 
--- see: https://github.com/rcarriga/nvim-notify?tab=readme-ov-file#usage
--- @type fun(): nil
-M.inspect = function(value) vim.notify(vim.inspect(value), vim.log.levels.INFO) end
-
--- For extending a list-type table with another list-type table only (not a dictionary).
--- @type fun(list:table, values:table): table
+---Extends a list-type table with another list-type table only (not a dictionary).
+---@type fun(list:table, values:table): table
 M.extend = function(list, items) vim.list_extend(list or {}, items) end
 
 M.get_system_executable_path = function(executable_name)
