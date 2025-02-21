693fface7613ec56435d0263af7b5c8ce0633728
Michael Uloth <hello@michaeluloth.com>
2025-02-20 20:53:14 -0500

    dap: add todos for splitting up file

--- lua/config/plugins/todo/debugging/dap.lua
+++ lua/config/plugins/todo/debugging/dap.lua
@@ -1,7 +1,4 @@
 -- see: https://www.lazyvim.org/extras/dap/core#nvim-dap
-local prefer_venv_executable = require('util.prefer_venv').prefer_venv_executable
-
-local js_based_languages = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact', 'vue' }
 
 local attach_debugger = function()
   -- (re-)reads launch.json if present
@@ -17,6 +14,9 @@ local attach_debugger = function()
     --       request = 'launch',
     --     },
 
+    local js_based_languages = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact', 'vue' }
+
+    -- TODO: move to typescript.lua?
     require('dap.ext.vscode').load_launchjs(nil, {
       ['pwa-node'] = js_based_languages,
       ['chrome'] = js_based_languages,
@@ -25,18 +25,19 @@ local attach_debugger = function()
 
     local dap_configurations = require('dap').configurations
 
-    -- override console in all configurations
+    -- override console in all configurations (including launch.json)
     for _, config in ipairs(dap_configurations) do
       config.console = 'integratedTerminal'
       -- config.justMyCode = true  -- NOTE: sometimes it helps to follow code into dependencies we built
     end
 
+    -- TODO: can I move this to python.lua?
     -- override pythonPath in all python configurations
-    local python_configurations = dap_configurations.python or {}
-    local python = prefer_venv_executable('python')
-    for _, config in ipairs(python_configurations) do
-      config.pythonPath = python
-    end
+    -- local python_configurations = dap_configurations.python or {}
+    -- local python = prefer_venv_executable('python')
+    -- for _, config in ipairs(python_configurations) do
+    --   config.pythonPath = python
+    -- end
   end
 
   require('dap').continue()
@@ -46,6 +47,7 @@ return {
   'mfussenegger/nvim-dap',
   dependencies = {
     {
+      -- TODO: move this spec to its own file and just list the repo name here
       'rcarriga/nvim-dap-ui',
       opts = {
         -- see: https://github.com/rcarriga/nvim-dap-ui/blob/master/lua/dapui/config/init.lua
@@ -111,12 +113,14 @@ return {
 
     -- virtual text for the debugger
     {
+      -- TODO: move this spec to its own file and just include the repo name here
       'theHamsta/nvim-dap-virtual-text',
       opts = {},
     },
 
     {
       'folke/which-key.nvim',
+      -- TODO: move this option to which-key.lua and just include the repo name here
       opts = {
         defaults = {
           ['<leader>d'] = { name = 'Debug' },
@@ -124,42 +128,44 @@ return {
       },
     },
 
-  keys = {
-    -- { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
-    { '<leader>db', function() require('dap').toggle_breakpoint() end, desc = 'Toggle Breakpoint' },
-    {
-      '<leader>dB',
-      function() require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: ')) end,
-      desc = 'Breakpoint Condition',
+    keys = {
+      -- { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
+      { '<leader>db', function() require('dap').toggle_breakpoint() end, desc = 'Toggle Breakpoint' },
+      {
+        '<leader>dB',
+        function() require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: ')) end,
+        desc = 'Breakpoint Condition',
+      },
+      { '<leader>dc', function() require('dap').continue() end, desc = 'Continue' },
+      { '<leader>dC', function() require('dap').run_to_cursor() end, desc = 'Run to Cursor' },
+      { '<leader>dd', attach_debugger, desc = 'Start debugger' },
+      { '<leader>de', function() require('dapui').eval() end, desc = 'Eval', mode = { 'n', 'v' } },
+      { '<leader>dg', function() require('dap').goto_() end, desc = 'Go to line (no execute)' },
+      { '<leader>di', function() require('dap').step_into() end, desc = 'Step Into' },
+      { '<leader>dj', function() require('dap').down() end, desc = 'Move down stack' },
+      { '<leader>dk', function() require('dap').up() end, desc = 'Move up stack' },
+      { '<leader>dl', function() require('dap').run_last() end, desc = 'Run Last' },
+      { '<leader>do', function() require('dap').step_out() end, desc = 'Step Out' },
+      { '<leader>dp', function() require('dap').pause() end, desc = 'Pause' },
+      { '<leader>dr', function() require('dap').restart() end, desc = 'Restart' },
+      { '<leader>ds', function() require('dap').step_over() end, desc = 'Step Over' },
+      { '<leader>dx', function() require('dap').terminate() end, desc = 'End session' },
+      { '<leader>du', function() require('dapui').toggle({}) end, desc = 'Dap UI' },
+      { '<leader>dw', function() require('dapui').elements.watches.add() end, desc = 'Watch symbol under cursor' },
     },
-    { '<leader>dc', function() require('dap').continue() end, desc = 'Continue' },
-    { '<leader>dC', function() require('dap').run_to_cursor() end, desc = 'Run to Cursor' },
-    { '<leader>dd', attach_debugger, desc = 'Start debugger' },
-    { '<leader>de', function() require('dapui').eval() end, desc = 'Eval', mode = { 'n', 'v' } },
-    { '<leader>dg', function() require('dap').goto_() end, desc = 'Go to line (no execute)' },
-    { '<leader>di', function() require('dap').step_into() end, desc = 'Step Into' },
-    { '<leader>dj', function() require('dap').down() end, desc = 'Move down stack' },
-    { '<leader>dk', function() require('dap').up() end, desc = 'Move up stack' },
-    { '<leader>dl', function() require('dap').run_last() end, desc = 'Run Last' },
-    { '<leader>do', function() require('dap').step_out() end, desc = 'Step Out' },
-    { '<leader>dp', function() require('dap').pause() end, desc = 'Pause' },
-    { '<leader>dr', function() require('dap').restart() end, desc = 'Restart' },
-    { '<leader>ds', function() require('dap').step_over() end, desc = 'Step Over' },
-    { '<leader>dx', function() require('dap').terminate() end, desc = 'End session' },
-    { '<leader>du', function() require('dapui').toggle({}) end, desc = 'Dap UI' },
-    { '<leader>dw', function() require('dapui').elements.watches.add() end, desc = 'Watch symbol under cursor' },
+
+    config = function()
+      -- TODO: replace lazyvim import?
+      local Config = require('lazyvim.config')
+      vim.api.nvim_set_hl(0, 'DapStoppedLine', { default = true, link = 'Visual' })
+
+      for name, sign in pairs(Config.icons.dap) do
+        sign = type(sign) == 'table' and sign or { sign }
+        vim.fn.sign_define(
+          'Dap' .. name,
+          { text = sign[1], texthl = sign[2] or 'DiagnosticInfo', linehl = sign[3], numhl = sign[3] }
+        )
+      end
+    end,
   },
-
-  config = function()
-    local Config = require('lazyvim.config')
-    vim.api.nvim_set_hl(0, 'DapStoppedLine', { default = true, link = 'Visual' })
-
-    for name, sign in pairs(Config.icons.dap) do
-      sign = type(sign) == 'table' and sign or { sign }
-      vim.fn.sign_define(
-        'Dap' .. name,
-        { text = sign[1], texthl = sign[2] or 'DiagnosticInfo', linehl = sign[3], numhl = sign[3] }
-      )
-    end
-  end,
 }
