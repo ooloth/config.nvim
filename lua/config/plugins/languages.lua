425706cb621f8cef0e6636db3d82902b5d8e531d
Michael Uloth <hello@michaeluloth.com>
2025-02-20 20:52:09 -0500

    langs: remove intermediate variable

--- lua/config/plugins/languages.lua
+++ lua/config/plugins/languages.lua
@@ -1,5 +1,3 @@
 -- TODO: enable inlay hints and code lenses for each LSP server
 
-local util = require('config.util')
-
-return util.require_all_files_in_config_directory('plugins/lang')
+return require('config.util').require_all_files_in_config_directory('plugins/lang')
