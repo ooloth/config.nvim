67ad0ebe1c3815448222caf2f43c423c4f909ced
Michael Uloth <hello@michaeluloth.com>
2025-02-26 00:19:07 +0000

    plenary: ensure latest version is installed

---
+++ lua/config/plugins/specs/plenary.lua
@@ -0,0 +1,4 @@
+return {
+  'nvim-lua/plenary.nvim',
+  branch = 'master', -- resolves "buffer not modifiable" after first chat question: https://github.com/olimorris/codecompanion.nvim/issues/377#issuecomment-2443121875
+}
