b8548570aa02c3eaddc694692446c8293136d499
Michael Uloth <hello@michaeluloth.com>
2025-02-28 15:13:04 -0500

    vim-tmux-navigator: remove n/p keymaps since issue is on tmux side, not vim side

--- lua/config/plugins/specs/vim-tmux-navigator.lua
+++ lua/config/plugins/specs/vim-tmux-navigator.lua
@@ -14,9 +14,7 @@ return {
     { '<c-h>', '<cmd>TmuxNavigateLeft<cr>', mode = { 'n', 'v' } },
     { '<c-l>', '<cmd>TmuxNavigateRight<cr>', mode = { 'n', 'v' } },
     { '<c-k>', '<cmd>TmuxNavigateUp<cr>', mode = { 'n', 'v' } },
-    { '<c-p>', '<cmd>TmuxNavigateUp<cr>', mode = { 'n', 'v' } }, -- compensate for c-k outputting c-p on Air
     { '<c-j>', '<cmd>TmuxNavigateDown<cr>', mode = { 'n', 'v' } },
-    { '<c-n>', '<cmd>TmuxNavigateDown<cr>', mode = { 'n', 'v' } }, -- compensate for c-j outputting c-n on Air
     { '<c-\\>', '<cmd>TmuxNavigatePrevious<cr>', mode = { 'n', 'v' } },
   },
 }
