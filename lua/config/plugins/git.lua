10538c54d38d8088e6518a3ad319ba306f6c09f6
Michael Uloth <hello@michaeluloth.com>
2025-02-28 16:14:49 -0500

    git: add snacks-gitbrowse for opening selection on github

--- lua/config/plugins/git.lua
+++ lua/config/plugins/git.lua
@@ -1,4 +1,5 @@
 return {
   require('config.plugins.specs.gitsigns'), -- show git modification symbols in the sign column
+  require('config.plugins.specs.snacks-gitbrowse'), -- open selection on github
   require('config.plugins.specs.snacks-lazygit'),
 }
---
+++ lua/config/plugins/specs/snacks-gitbrowse.lua
@@ -0,0 +1,17 @@
+---@module 'snacks'
+
+return {
+  'folke/snacks.nvim',
+  opts = {
+    gitbrowse = {
+      -- https://github.com/folke/snacks.nvim/blob/main/docs/gitbrowse.md#%EF%B8%8F-config
+    },
+  },
+  -- stylua: ignore
+  keys = {
+    { '<leader>go', function() Snacks.gitbrowse.open({ what = 'commit' }) end, desc = 'Open on GitHub', mode = { 'n', 'v' } },
+    { '<leader>gO', function() Snacks.gitbrowse.open({ what = 'permalink' }) end, desc = 'Open on GitHub (permalink)', mode = { 'n', 'v' } },
+    { '<leader>gy', function() Snacks.gitbrowse.open({ what = 'commit' }) end, desc = 'Open on GitHub', mode = { 'n', 'v' } },
+    { '<leader>gY', function() Snacks.gitbrowse.open({ what = 'permalink' }) end, desc = 'Open on GitHub (permalink)', mode = { 'n', 'v' } },
+  },
+}
