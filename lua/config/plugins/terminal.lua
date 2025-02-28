3eabc66baff543594d61d0f7f27c40f24475b4b0
Michael Uloth <hello@michaeluloth.com>
2025-02-28 15:13:37 -0500

    terminal: <esc><esc> to enter normal mode in terminal buffer (not one <esc> which is vi mode in zsh)

--- lua/config/plugins/terminal.lua
+++ lua/config/plugins/terminal.lua
@@ -1,6 +1,6 @@
 local set = vim.keymap.set
 
-set('t', '<esc>', '<c-\\><c-n>', { desc = 'Enter normal mode' })
+set('t', '<esc><esc>', '<c-\\><c-n>', { desc = 'Enter normal mode' })
 set('t', '<c-/>', '<cmd>close<cr>', { desc = 'Hide Terminal' })
 set('t', '<c-_>', '<cmd>close<cr>', { desc = 'which_key_ignore' })
 
