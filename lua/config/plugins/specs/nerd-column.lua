598f8b2b2243d79728abba5d0f24674ece1a1bac
Michael Uloth <hello@michaeluloth.com>
2025-03-06 12:45:51 -0500

    nerd-column: disable more filetypes by default

--- lua/config/plugins/specs/nerd-column.lua
+++ lua/config/plugins/specs/nerd-column.lua
@@ -1,13 +1,29 @@
 return {
   'hankertrix/nerd_column.nvim',
   event = { 'BufEnter' },
-  opts = {
-    colour_column = 120,
-    custom_colour_column = {
-      lua = 130,
-    },
-    disabled_filetypes = { 'help', 'text', 'markdown' },
-    respect_editor_config = true,
-    scope = 'file',
-  },
+  config = function()
+    -- https://github.com/hankertrix/nerd_column.nvim?tab=readme-ov-file#default-configuration
+    local default_config = require('nerd_column').default_config or {}
+
+    ---@type NerdColumn.Config
+    local opts = {
+      colour_column = 999, -- default to no colorcolumn appearing
+      custom_colour_column = {
+        astro = 120, -- TODO: derive from .prettierrc? or what conform.nvim knows?
+        javascript = 120, -- TODO: derive from .prettierrc? or what conform.nvim knows?
+        javascriptreact = 120, -- TODO: derive from .prettierrc? or what conform.nvim knows?
+        lua = 130, -- TODO: derive from .stylua.toml? or what conform.nvim knows?
+        python = 120, -- TODO: derive from pyproject.toml? or what conform.nvim knows?
+        typescript = 120, -- TODO: derive from .prettierrc? or what conform.nvim knows?
+        typescriptreact = 120, -- TODO: derive from .prettierrc? or what conform.nvim knows?
+      },
+      disabled_file_types = vim.list_extend(
+        default_config.disabled_file_types,
+        { 'c', 'css', 'csv', 'markdown', 'text', 'tmux', 'yaml' }
+      ),
+      scope = 'line',
+    }
+
+    require('nerd_column').setup(opts)
+  end,
 }
