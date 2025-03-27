e7f25d6804c5775d5a5f639d6eb6b2dcf0978c2a
Michael Uloth <hello@michaeluloth.com>
2025-03-26 20:08:35 -0400

    ui: leader-on to toggle line numbers

--- lua/config/plugins/ui.lua
+++ lua/config/plugins/ui.lua
@@ -40,6 +40,9 @@ vim.opt.wrap = false -- disable line wrap
 local set = vim.keymap.set
 local autocmd = vim.api.nvim_create_autocmd
 
+-- Toggle line numbers (when hiding, hide relative numbers too)
+set('n', '<leader>on', '<cmd>set nu! nornu<cr>', { desc = 'Line numbers (toggle)' })
+
 -- Resize window using <ctrl> arrow keys
 set('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
 set('n', '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease Window Height' })
