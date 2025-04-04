c9383d8e3630c0c4caad248531f13046a7077224
Michael Uloth <hello@michaeluloth.com>
2025-04-03 23:33:04 -0400

    catppuccin: lighter dap virtual text (for readability)

--- lua/config/plugins/specs/catppuccin.lua
+++ lua/config/plugins/specs/catppuccin.lua
@@ -78,5 +78,8 @@ return {
     -- Subtle background instead of underline
     vim.api.nvim_set_hl(0, 'TreesitterContext', { bg = '#232333' }) -- 25% of way from base to surface0
     vim.api.nvim_set_hl(0, 'TreesitterContextBottom', { bg = '#232333' })
+
+    -- More noticeable than default comment color
+    vim.api.nvim_set_hl(0, 'NvimDapVirtualText', { fg = mocha.subtext0 })
   end,
 }
