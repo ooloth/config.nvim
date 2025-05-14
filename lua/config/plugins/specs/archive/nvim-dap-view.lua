e2829e5162b69ba453f1af2f416f4a01fe93912f
Michael Uloth <hello@michaeluloth.com>
2025-05-14 16:22:58 -0400

    dap-view: archive spec (not fully featured enough yet)

---
+++ lua/config/plugins/specs/archive/nvim-dap-view.lua
@@ -0,0 +1,30 @@
+---WARN: multiple debug sessions aren't supported at this point: https://github.com/igorlfs/nvim-dap-view/issues/50
+
+---@module 'dap-view'
+
+return {
+  'igorlfs/nvim-dap-view',
+  opts = {
+    winbar = {
+      sections = { 'console', 'repl', 'watches', 'scopes', 'exceptions', 'breakpoints', 'threads' },
+      default_section = 'console',
+      headers = {
+        breakpoints = 'Breakpoints [B]',
+        console = 'Terminal [T]',
+        exceptions = 'Exceptions [E]',
+        repl = 'REPL [R]',
+        scopes = 'Variables [V]',
+        threads = 'Stack frames [S]',
+        watches = 'Watches [W]',
+      },
+    },
+  },
+  config = function(_, opts)
+    local dap, dv = require('dap'), require('dap-view')
+    dv.setup(opts)
+    dap.listeners.before.attach['dap-view-config'] = dv.open
+    dap.listeners.before.launch['dap-view-config'] = dv.open
+    dap.listeners.before.event_terminated['dap-view-config'] = dv.close
+    dap.listeners.before.event_exited['dap-view-config'] = dv.close
+  end,
+}
--- lua/config/plugins/specs/nvim-dap-view.lua
+++
@@ -1,6 +0,0 @@
----@module 'dap-view'
-
-return {
-  'igorlfs/nvim-dap-view',
-  opts = {},
-}
