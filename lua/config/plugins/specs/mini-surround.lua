4bf1a96155036cf81d809646ee997dcce3372fa3
Michael Uloth <hello@michaeluloth.com>
2025-05-18 22:30:37 -0400

    editing: sa/sd/sf/sr to modify surrounding character pairs

--- lua/config/plugins/specs/mini-surround.lua
+++ lua/config/plugins/specs/mini-surround.lua
@@ -7,19 +7,42 @@ return {
   'echasnovski/mini.surround',
   recommended = true,
   opts = {
+    -- Add custom surroundings to be used on top of builtin ones. For more
+    -- information with examples, see `:h MiniSurround.config`.
+    custom_surroundings = nil,
+
+    -- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
+    highlight_duration = 500,
+
     mappings = {
-      add = 'gsa', -- e.g. gsaiw' (normal + visual mode) Add surrounding in Normal and Visual modes
-      delete = 'gsd', -- e.g. gsd' (normal mode)
-      replace = 'gsr', -- e.g. gsr'" (normal mode)
-      -- add = 'gs', -- e.g. gsiw' (normal + visual mode)
-      -- delete = 'ds', -- e.g. ds' (normal mode)
-      -- replace = 'cs', -- e.g. cs'" (normal mode)
-      find = '', -- disable
-      find_left = '', -- disable
-      highlight = '', -- disable
-      update_n_lines = '', -- disable
-      suffix_last = '', -- disable
-      suffix_next = '', -- disable
+      add = 'sa', -- Add surrounding in Normal and Visual modes
+      delete = 'sd', -- Delete surrounding
+      find = 'sf', -- Find surrounding (to the right)
+      find_left = 'sF', -- Find surrounding (to the left)
+      highlight = 'sh', -- Highlight surrounding
+      replace = 'sr', -- Replace surrounding
+      update_n_lines = 'sn', -- Update `n_lines`
+      suffix_last = 'l', -- Suffix to search with "prev" method
+      suffix_next = 'n', -- Suffix to search with "next" method
     },
+
+    -- Number of lines within which surrounding is searched
+    n_lines = 20,
+
+    -- Whether to respect selection type:
+    -- - Place surroundings on separate lines in linewise mode.
+    -- - Place surroundings on each line in blockwise mode.
+    respect_selection_type = false,
+
+    -- How to search for surrounding (first inside current line, then inside
+    -- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
+    -- 'cover_or_nearest', 'next', 'prev', 'nearest'. For more details,
+    -- see `:h MiniSurround.config`.
+    search_method = 'cover',
+
+    -- Whether to disable showing non-error feedback
+    -- This also affects (purely informational) helper messages shown after
+    -- idle time if user input is required.
+    silent = false,
   },
 }
