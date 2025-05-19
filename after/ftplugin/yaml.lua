17f9ca517aab91e4a82a07711ea8acb907b7485a
Michael Uloth <hello@michaeluloth.com>
2025-05-19 19:19:05 -0400

    yaml: always use a tab size of 2 spaces, and convert to spaces

---
+++ after/ftplugin/yaml.lua
@@ -0,0 +1,4 @@
+vim.opt_local.expandtab = true -- replace tabs with spaces
+vim.opt_local.shiftwidth = 2 -- indent size
+vim.opt_local.smartindent = true -- insert indents automatically
+vim.opt_local.tabstop = 2 -- tab size
