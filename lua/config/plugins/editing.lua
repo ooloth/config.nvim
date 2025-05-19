4ddb99093ba678ba5e92f136a3b1d8f43d99088e
Michael Uloth <hello@michaeluloth.com>
2025-05-18 22:02:09 -0400

    editing: leader-rs to start a full-buffer substitute command

--- lua/config/plugins/editing.lua
+++ lua/config/plugins/editing.lua
@@ -30,6 +30,8 @@ local set = vim.keymap.set
 -- new file
 set('n', '<leader>fn', ':ene <BAR> startinsert<cr>', { desc = 'New file' })
 
+set('n', '<leader>rs', ':%s/', { desc = 'Substitute' })
+
 -- better indenting
 set('v', '<', '<gv')
 set('v', '>', '>gv')
