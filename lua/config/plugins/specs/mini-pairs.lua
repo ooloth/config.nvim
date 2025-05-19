08d67182b42af5159f305830a621e67c8139ee89
Michael Uloth <hello@michaeluloth.com>
2025-05-19 19:02:54 -0400

    mini-pairs: skip closing pair if right next to a word

--- lua/config/plugins/specs/mini-pairs.lua
+++ lua/config/plugins/specs/mini-pairs.lua
@@ -9,14 +9,25 @@
 return {
   'echasnovski/mini.pairs',
   opts = {
+    mappings = {
+      -- See: https://github.com/echasnovski/mini.nvim/issues/835
+      -- Prevents the action if the cursor is just before any character or next to a "\".
+      ['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\][%s%)%]%}]' },
+      ['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\][%s%)%]%}]' },
+      ['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\][%s%)%]%}]' },
+      -- This is default (prevents the action if the cursor is just next to a "\").
+      [')'] = { action = 'close', pair = '()', neigh_pattern = '[^\\].' },
+      [']'] = { action = 'close', pair = '[]', neigh_pattern = '[^\\].' },
+      ['}'] = { action = 'close', pair = '{}', neigh_pattern = '[^\\].' },
+      -- Prevents the action if the cursor is just before or next to any character.
+      ['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '[^%w][^%w]', register = { cr = false } },
+      ["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[^%w][^%w]', register = { cr = false } },
+      ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^%w][^%w]', register = { cr = false } },
+    },
     modes = {
       insert = true,
       command = true,
       terminal = false,
     },
-    skip_next = [=[[%w%%%'%[%"%.%`%$]]=], -- skip autopair when next character is one of these
-    skip_ts = { 'string' }, -- skip autopair when the cursor is inside these treesitter nodes
-    skip_unbalanced = true, -- skip autopair when next character is closing pair and there are more closing pairs than opening pairs
-    markdown = true, -- better deal with markdown code blocks
   },
 }
