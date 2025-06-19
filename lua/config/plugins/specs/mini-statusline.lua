b5c0a211ad9597391384ae09bc1b7f057284371a
Michael Uloth <hello@michaeluloth.com>
2025-06-18 21:56:28 -0400

    statusline: debugger "paused" instead of "stopped"

--- lua/config/plugins/specs/mini-statusline.lua
+++ lua/config/plugins/specs/mini-statusline.lua
@@ -22,9 +22,8 @@ local get_debugger_status = function()
   local summary = status:lower()
   if status:find('Starting') then summary = 'running' end
   if status:find('Running') then summary = 'running' end
-  if status:find('Stopped') then summary = 'stopped' end
-  if status:find('stopped') then summary = 'stopped' end
-  -- if status:find('Stopped at line') then summary = 'stopped at L' .. status:match('(%d+)') end
+  if status:find('Stopped') then summary = 'paused' end
+  if status:find('stopped') then summary = 'paused' end
 
   return 'Debugger ' .. summary
 end
