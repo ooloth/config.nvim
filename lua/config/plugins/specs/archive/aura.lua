593b6e22c4f82dd110197ddd0f91df4e9ed26987
Michael Uloth <hello@michaeluloth.com>
2025-06-18 21:57:47 -0400

    aura: archive spec

---
+++ lua/config/plugins/specs/archive/aura.lua
@@ -0,0 +1,9 @@
+return {
+  'daltonmenezes/aura-theme',
+  lazy = false,
+  priority = 1000,
+  config = function(plugin)
+    vim.opt.rtp:append(plugin.dir .. '/packages/neovim')
+    vim.cmd([[colorscheme aura-dark]])
+  end,
+}
