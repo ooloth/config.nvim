3b4b40ce343aca6ea1f87438652236af27c930b2
Michael Uloth <hello@michaeluloth.com>
2025-08-26 14:54:23 -0400

    smartyank: archive config

--- lua/config/plugins/specs/archive/smartyank.lua
+++ lua/config/plugins/specs/archive/smartyank.lua
@@ -10,11 +10,3 @@ return {
     },
   },
 }
-
--- NOTE: add back to editing.lua or ui.lua if I ever stop using this plugin
--- autocmd('TextYankPost', {
---   desc = 'Highlight yanked text',
---   callback = function()
---     vim.hl.on_yank({ higroup = 'Visual', timeout = 200 })
---   end,
--- })
