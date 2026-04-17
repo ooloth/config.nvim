b090b3115f7ba6f5111f56bf89d840c4614870ce
Michael Uloth <hello@michaeluloth.com>
2026-04-16 22:31:59 -0400

    lang/astro,vue: configure typescript sdk and plugin paths
    astro-ls silently does nothing without init_options.typescript.tsdk
    pointing at a TypeScript installation. vue_ls (Volar v2) requires
    ts_ls to attach alongside it on .vue files, which needs ts_ls to
    have vue in its filetypes and @vue/typescript-plugin in its
    init_options.plugins. Both resolve from node_modules relative to
    getcwd() at startup, so nvim should be opened from the project root.

--- lua/config/plugins/lang/astro.lua
+++ lua/config/plugins/lang/astro.lua
@@ -1,4 +1,12 @@
 -- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#astro
+-- astro-ls requires the TypeScript SDK path to function; falls back gracefully if not in project
+vim.lsp.config('astro', {
+  init_options = {
+    typescript = {
+      tsdk = vim.fn.getcwd() .. '/node_modules/typescript/lib',
+    },
+  },
+})
 vim.lsp.enable('astro')
 
 return {
--- lua/config/plugins/lang/vue.lua
+++ lua/config/plugins/lang/vue.lua
@@ -1,7 +1,24 @@
 -- TODO: https://www.lazyvim.org/extras/lang/vue
 
--- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#vuels
-vim.lsp.enable('vuels')
+-- vue_ls (Volar v2) requires ts_ls to attach alongside it on .vue files.
+-- This extends ts_ls to handle vue filetypes and loads @vue/typescript-plugin.
+-- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#vue_ls
+local function vue_ts_plugin_location()
+  local path = vim.fn.getcwd() .. '/node_modules/@vue/typescript-plugin'
+  if vim.fn.isdirectory(path) == 1 then return path end
+  return ''
+end
+
+vim.lsp.config('ts_ls', {
+  -- must include the full default filetypes list since arrays replace rather than merge
+  filetypes = { 'javascript', 'javascriptreact', 'javascript.jsx', 'typescript', 'typescriptreact', 'typescript.tsx', 'vue' },
+  init_options = {
+    plugins = {
+      { name = '@vue/typescript-plugin', location = vue_ts_plugin_location(), languages = { 'vue' } },
+    },
+  },
+})
+vim.lsp.enable('vue_ls')
 
 return {
   {
