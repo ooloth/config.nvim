24fc5bcd68d8ee6e4cb9241f84574d617486fbde
Michael Uloth <hello@michaeluloth.com>
2026-04-27 09:20:14 -0400

    nerd-column: tighten colorcolumn to 100 and shrink disabled filetypes
    Changed all custom_colour_column values from 120/130 to 100 to match
    the common Prettier printWidth default. Removed file types from the
    disabled list (css, docker, gleam, go, graphql, markdown, pug, rego,
    ruby, rust, shell, sql, svelte, terraform, text, tmux, toml, vue) so
    the colorcolumn now appears in those buffers too.

--- lua/config/plugins/specs/nerd-column.lua
+++ lua/config/plugins/specs/nerd-column.lua
@@ -9,43 +9,25 @@ return {
     local opts = {
       colour_column = 999, -- default to no colorcolumn appearing
       custom_colour_column = {
-        astro = 120, -- TODO: derive from .prettierrc? or what conform.nvim knows?
-        javascript = 120, -- TODO: derive from .prettierrc? or what conform.nvim knows?
-        javascriptreact = 120, -- TODO: derive from .prettierrc? or what conform.nvim knows?
-        lua = 130, -- TODO: derive from .stylua.toml? or what conform.nvim knows?
-        python = 120, -- TODO: derive from pyproject.toml? or what conform.nvim knows?
-        typescript = 120, -- TODO: derive from .prettierrc? or what conform.nvim knows?
-        typescriptreact = 120, -- TODO: derive from .prettierrc? or what conform.nvim knows?
+        astro = 100, -- TODO: derive from .prettierrc? or what conform.nvim knows?
+        javascript = 100, -- TODO: derive from .prettierrc? or what conform.nvim knows?
+        javascriptreact = 100, -- TODO: derive from .prettierrc? or what conform.nvim knows?
+        lua = 100, -- TODO: derive from .stylua.toml? or what conform.nvim knows?
+        python = 100, -- TODO: derive from pyproject.toml? or what conform.nvim knows?
+        typescript = 100, -- TODO: derive from .prettierrc? or what conform.nvim knows?
+        typescriptreact = 100, -- TODO: derive from .prettierrc? or what conform.nvim knows?
       },
       disabled_file_types = vim.list_extend(default_config.disabled_file_types, {
         'c',
-        'css',
         'csv',
         'deno',
-        'docker',
         'git',
-        'gleam',
-        'go',
-        'graphql',
         'helm',
         'html',
         'json',
-        'markdown',
         'perl',
         'protobuf',
-        'pug',
-        'rego',
-        'ruby',
-        'rust',
-        'shell',
-        'sql',
-        'svelte',
-        'terraform',
-        'text',
-        'tmux',
-        'toml',
         'vim',
-        'vue',
         'xml',
         'yaml',
       }),
